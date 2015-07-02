c1be8344868f27f13c451465751978fdd4ac90b8
Leanne Goddard <leanne.goddard@mnscorp.net>
2015-07-02 11:32:20 +0100

    webtemplate controller and routing added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,22 @@
-//js goes here
+'use strict';
+
+angular
+    .module('templateApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+  
